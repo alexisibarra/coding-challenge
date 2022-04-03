df8deedd779686ce92befd44c66ed7c8e3859797
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2022-04-03 15:26:24 -0400

    update log-source unit tests

--- __tests__/log-source.jest.unit.ts
+++ __tests__/log-source.jest.unit.ts
@@ -1,14 +1,14 @@
-const LogSource = require("../lib/log-source");
+import LogSource from "../lib/log-source";
 
 describe("Log Source Behaviors", () => {
   test("It should synchronously drain a log source", () => {
     const source = new LogSource();
     let entry = source.pop();
-    expect(new Date() > entry.date).toBeTruthy();
-    expect(entry.msg).toBeTruthy();
+    expect(entry && new Date() > entry.date).toBeTruthy();
+    expect(entry && entry.msg).toBeTruthy();
     entry = source.pop();
-    expect(new Date() > entry.date).toBeTruthy();
-    expect(entry.msg).toBeTruthy();
+    expect(entry && new Date() > entry.date).toBeTruthy();
+    expect(entry && entry.msg).toBeTruthy();
     source.last.date = new Date();
     entry = source.pop();
     expect(entry).toBeFalsy();
@@ -17,11 +17,11 @@ describe("Log Source Behaviors", () => {
   test("It should asynchronously drain a log source", async () => {
     const source = new LogSource();
     let entry = await source.popAsync();
-    expect(new Date() > entry.date).toBeTruthy();
-    expect(entry.msg).toBeTruthy();
+    expect(entry && new Date() > entry.date).toBeTruthy();
+    expect(entry && entry.msg).toBeTruthy();
     entry = await source.popAsync();
-    expect(new Date() > entry.date).toBeTruthy();
-    expect(entry.msg).toBeTruthy();
+    expect(entry && new Date() > entry.date).toBeTruthy();
+    expect(entry && entry.msg).toBeTruthy();
     source.last.date = new Date();
     entry = await source.popAsync();
     expect(entry).toBeFalsy();
